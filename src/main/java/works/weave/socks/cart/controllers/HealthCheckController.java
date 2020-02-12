5ae9a41fa4de033bfb0aff678e69b597589ef255
bfry96 <brandon.fry@dynatrace.com>
2020-02-12 20:16:05 +0000

    fixed dumb space

--- src/main/java/works/weave/socks/cart/controllers/HealthCheckController.java
+++ src/main/java/works/weave/socks/cart/controllers/HealthCheckController.java
@@ -40,7 +40,7 @@ public class HealthCheckController {
        }
 
        healthChecks.add(app);
-      healthChecks.add(database);
+       healthChecks.add(database);
 
        map.put("health", healthChecks);
        return map;
